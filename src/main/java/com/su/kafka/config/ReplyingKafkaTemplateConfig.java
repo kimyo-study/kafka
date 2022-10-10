704fdb95611cf6770cf6cb594cab00097f87e7db
kimziou77 <kimziou77@naver.com>
2022-10-10 16:44:06 +0900

    feat: replyingKafkaTemplate practice

---
+++ src/main/java/com/su/kafka/config/ReplyingKafkaTemplateConfig.java
@@ -0,0 +1,27 @@
+package com.su.kafka.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+
+@Configuration
+public class ReplyingKafkaTemplateConfig {
+
+    @Bean
+    public ReplyingKafkaTemplate<String,String,String> replyingKafkaTemplate(
+        ProducerFactory<String, String> producerFactory,
+        ConcurrentMessageListenerContainer<String, String> repliesContainer
+    ){
+        return new ReplyingKafkaTemplate<>(producerFactory,repliesContainer);
+    }
+
+    @Bean
+    public ConcurrentMessageListenerContainer<String, String> repliesContainer(ConcurrentKafkaListenerContainerFactory<String,String> containerFactory) {
+        var container = containerFactory.createContainer("clip3-replies");
+        container.getContainerProperties().setGroupId("clip3-replies-container-id");
+        return container;
+    }
+}
