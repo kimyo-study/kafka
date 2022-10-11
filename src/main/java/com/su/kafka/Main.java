247da64a5fb834f24d3b046f4b2958c75819ecee
kimziou77 <kimziou77@naver.com>
2022-10-11 11:09:07 +0900

    feat: clip-consumer

--- src/main/java/com/su/kafka/Main.java
+++ src/main/java/com/su/kafka/Main.java
@@ -4,7 +4,9 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
+import com.su.kafka.service.ClipConsumer;
 import com.su.kafka.service.KafkaManager;
 
 @SpringBootApplication
@@ -15,12 +17,19 @@ public class Main {
     }
 
     @Bean
-    public ApplicationRunner runner(KafkaManager kafkaManager){
+    public ApplicationRunner runner(
+        KafkaManager kafkaManager,
+        KafkaTemplate<String, String>kafkaTemplate,
+        ClipConsumer clipConsumer
+    ){
         return args -> {
             kafkaManager.describeTopicConfigs();
             kafkaManager.changeConfig();
             kafkaManager.findAllConsumerGroups();
             kafkaManager.findAllOffsets();
+
+            kafkaTemplate.send("clip1-listener", "Hello, Listener");
+            clipConsumer.seek();
         };
     }
 }
